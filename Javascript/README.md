dc9826a2b3b37b58bf7a969e9229d873e6381baa
Melvin <vmelvin299@gmail.com>
2025-11-01 00:46:12 +0530

    Day 2 done

--- Javascript/README.md
+++ Javascript/README.md
@@ -2,3 +2,5 @@ Starting Javascript from https://javascript.info/ on 30/10/25.<br />
 Test commit.<br />
 30/10/25<br />
 1.1 to 2.4 <br />
+31/10/25 <br />
+2.5 to 2.6 <br />
--- Javascript/prac2.js
+++ Javascript/prac2.js
@@ -7,3 +7,18 @@ message=123;
 alert(message);
 
 alert("ball" ** 0);
+alert(9007199254740991 + 3);
+
+let big = 1234567890n;
+
+let Name = 'Melvin';
+big = `You entered ${Name} as your name`;
+alert(big);
+
+let isCorrect = 1>2;
+
+typeof isCorrect;
+alert(typeof isCorrect);
+
+let age = prompt('please enter your age:');
+alert(`you entered your age as: ${age}`);
