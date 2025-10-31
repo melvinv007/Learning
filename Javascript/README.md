bcbe7cc37925550d2524a2924000d3dc92861fd6
Melvin <vmelvin299@gmail.com>
2025-10-31 22:22:47 +0530

    Restarting repo

---
+++ Javascript/README.md
@@ -0,0 +1,4 @@
+Starting Javascript from https://javascript.info/ on 30/10/25.<br />
+Test commit.<br />
+30/10/25<br />
+"/t"1.1 to 2.4
---
+++ Javascript/prac1.js
@@ -0,0 +1,9 @@
+"use strict";
+alert(1);
+let age=25 , name = "Melvin" , text;
+let message = "Hi, How are you";
+text = message;
+alert(message);
+alert(text);
+message = "Hope you are fine";
+alert(text);
