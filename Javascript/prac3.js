3bc7e80b8082deaa024fa5ff84bb7ac649e1caf6
Melvin <vmelvin299@gmail.com>
2025-11-01 18:59:51 +0530

    Done for now

---
+++ Javascript/prac3.js
@@ -0,0 +1,32 @@
+"use strict";
+let value = 1029;
+// value = String(value);
+
+// alert("   156        ");
+// alert(Number("   156        "));
+// alert(Boolean("")); // important
+
+//basic math operators
+
+// alert("1"+3);
+// alert(1+4+"5");
+// alert(6-'2');
+// alert(6/'2'/5);
+
+// let a = value + (value = value +1);
+// alert(a);
+
+// let counter = 1;
+// let b = ++counter; // (*)
+
+// alert(b); // 2
+// alert(counter);//2
+
+
+// let c = counter++; // (*)
+// alert(c); // 2
+// alert(counter);//3
+
+//comma 
+let a = (1+100,200);
+alert(a);
